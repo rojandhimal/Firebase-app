a910f1dbc2a3d1ec3ddd88940f95b4eb0f47ea1e
tegs1 <rojandhimal@crvn.net>
2023-01-18 20:37:51 +0545

    stats

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,8 +1,34 @@
+const { db } = require("../utils/auth");
+
 exports.indexView = async (req, res, next) => {
+  let totalStudents = 0;
+  let activeStudents = 0;
+  let cancelStudents = 0;
+
+  await db
+    .collection("students")
+    .get()
+    .then((snap) => {
+      totalStudents = snap.size;
+    });
+  await db
+    .collection("students")
+    .where("studentStatus", "==", "approved")
+    .get()
+    .then((snap) => {
+      activeBooking = snap.size;
+    });
+  await db
+    .collection("students")
+    .where("studentStatus", "==", "canceled")
+    .get()
+    .then((snap) => {
+      cancelBooking = snap.size;
+    });
+
   res.render("home", {
-    totalRestaurant: 0,
-    totalBooking: 0,
-    activeBooking: 0,
-    cancelBooking: 0,
+    totalStudents,
+    activeStudents,
+    cancelStudents
   });
 };
